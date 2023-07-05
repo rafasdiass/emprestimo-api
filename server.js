5a4fac0e11d4d25f9632fb3ed78463f6287af771
rafasdiass <128534833+rafasdiass@users.noreply.github.com>
2023-07-05 12:30:29 -0300

    validacao

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const Joi = require('@hapi/joi');
+const cpfCnpjValidator = require('cpf-cnpj-validator');
 const Loan = require('./Loan');
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -14,6 +16,7 @@ db.once('open', function() {
 });
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -25,30 +28,45 @@ app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
 
-
+// Definir o esquema de validação usando Joi
 const loanSchema = Joi.object({
     personType: Joi.string().valid('PF', 'PJ').required(),
-    document: Joi.string().required(),
+    document: Joi.string().custom((value, helper) => {
+      if (helper.state.ancestors[0].personType === 'PF') {
+        if (!cpfCnpjValidator.cpf.isValid(value)) {
+          return helper.message('CPF inválido');
+        }
+      } else if (helper.state.ancestors[0].personType === 'PJ') {
+        if (!cpfCnpjValidator.cnpj.isValid(value)) {
+          return helper.message('CNPJ inválido');
+        }
+      }
+      return true;
+    }, 'Validação de documento').required(),
     name: Joi.string().min(3).max(100).required(),
     documentNumber: Joi.string().required(),
     activeDebt: Joi.number().required(),
-    loanValue: Joi.number().max(50000).required()
+    loanValue: Joi.number().max(50000).custom((value, helper) => {
+      if (value > helper.state.ancestors[0].activeDebt / 2) {
+        return helper.message('O valor do empréstimo não pode ser maior que a metade da dívida ativa');
+      }
+      return true;
+    }, 'Validação de valor de empréstimo').required()
 });
 
 app.post('/loan', async (req, res) => {
-  const loan = new Loan({
-    personType: req.body.personType,
-    document: req.body.document,
-    name: req.body.name,
-    documentNumber: req.body.documentNumber,
-    activeDebt: req.body.activeDebt,
-    loanValue: req.body.loanValue,
-  });
+  const { error } = loanSchema.validate(req.body);
+  
+  if (error) return res.status(400).send(error.details[0].message);
 
+  const loan = new Loan(req.body);
+
+  // Aqui, adicionaremos a lógica para verificar se o empréstimo é válido de acordo com as regras que você forneceu.
+  
   try {
-    await loan.save();
-    res.send('Empréstimo aprovado');
+      await loan.save();
+      res.send('Empréstimo aprovado');
   } catch (err) {
-    res.status(400).send(err.message);
+      res.status(400).send(err);
   }
 });
